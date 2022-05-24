0b918f38d14a4db40c72275487cf4a244c6ab136
trm2418 <trm2418@rit.edu>
2022-05-23 20:10:58 -0400

    Update pokedex.ts

--- data/pokedex.ts
+++ data/pokedex.ts
@@ -5,7 +5,7 @@ export const Pokedex: {[speciesid: string]: SpeciesData} = {
 		types: ["Grass", "Poison"],
 		genderRatio: {M: 0.875, F: 0.125},
 		baseStats: {hp: 45, atk: 49, def: 49, spa: 65, spd: 65, spe: 45},
-		abilities: {0: "Overgrow", H: "Chlorophyll"},
+		abilities: {0: "Overgow", H: "Chlorophyll"},
 		heightm: 0.7,
 		weightkg: 6.9,
 		color: "Green",
