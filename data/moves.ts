611b75f9890ee1527d363955da059df5ff41b12a
trm2418 <trm2418@rit.edu>
2022-05-31 19:49:26 -0400

    Update moves.ts

--- data/moves.ts
+++ data/moves.ts
@@ -20170,7 +20170,7 @@ export const Moves: {[moveid: string]: MoveData} = {
 		flags: {},
 		volatileStatus: 'kindle',
 		onHit(pokemon) {
-			this.add('-start', pokemon, 'move: Kindle');
+			this.add('-singlemove', pokemon, 'move: Kindle');
 		},
 		condition: {
 			duration: 2,
